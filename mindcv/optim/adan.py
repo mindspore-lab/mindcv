fix prev gradient init

mindcv/optim/adan.py
@@ -132,19 +132,31 @@ def _update_run_op(beta1, beta2, beta3, eps, lr, weight_decay, param, m, v, n, g
     op_cast = P.Cast()
     op_reshape = P.Reshape()
     op_shape = P.Shape()
+
+    success = True
+
+    #if global_step == 0.0: # init 
+    # TODO: use global_step==0 as the condition to init prev_gradient as gradient 
+    #if (F.reduce_min(prev_gradient) == 0.0) and (F.reduce_max(prev_gradient) == 0.0):
+    if (F.reduce_sum(prev_gradient) == 0.0):
+        success = F.depend(success, F.assign(prev_gradient, gradient))
+
+
+    # TODO: is casting needed? 
     param_fp32 = op_cast(param, mstype.float32)
     m_fp32 = op_cast(m, mstype.float32)
     v_fp32 = op_cast(v, mstype.float32)
     n_fp32 = op_cast(n, mstype.float32)
     gradient_fp32 = op_cast(gradient, mstype.float32)
+    prev_gradient_fp32 = op_cast(prev_gradient, mstype.float32)
 
     next_m = op_mul(F.tuple_to_array((1.0,)) - beta1, m_fp32) + op_mul(beta1, gradient_fp32)
 
-    next_v = op_mul(F.tuple_to_array((1.0,)) - beta2, v_fp32) + op_mul(beta2, gradient - prev_gradient)
+    next_v = op_mul(F.tuple_to_array((1.0,)) - beta2, v_fp32) + op_mul(beta2, gradient_fp32 - prev_gradient_fp32)
 
     next_n = op_mul(F.tuple_to_array((1.0,)) - beta3, n_fp32) + op_mul(beta3, 
                     op_square(
-                        gradient + op_mul(F.tuple_to_array((1.0,)) - beta2, gradient - prev_gradient)
+                        gradient + op_mul(F.tuple_to_array((1.0,)) - beta2, gradient_fp32 - prev_gradient_fp32)
                         )
                     )
 
@@ -160,7 +172,6 @@ def _update_run_op(beta1, beta2, beta3, eps, lr, weight_decay, param, m, v, n, g
 
     next_param = next_param / (Tensor(1.0, mstype.float32)  + op_mul(weight_decay, lr))
 
-    success = True
     success= F.depend(success, F.assign(param, op_cast(next_param, F.dtype(param))))
     success= F.depend(success, F.assign(m, op_cast(next_m, F.dtype(m))))
     success= F.depend(success, F.assign(v, op_cast(next_v, F.dtype(v))))
@@ -189,8 +200,8 @@ class Adan(Optimizer):
     @opt_init_args_register
     def __init__(self, params, learning_rate=1e-3, beta1=0.98, beta2=0.92, beta3=0.99, eps=1e-8, use_locking=False,
                  weight_decay=1e-6, loss_scale=1.0):
-        super(Adan, self).__init__(learning_rate, params, weight_decay=weight_decay, loss_scale=loss_scale)
-        # TODO: now we use customized weight decay 
+        super(Adan, self).__init__(learning_rate, params, weight_decay=weight_decay, loss_scale=loss_scale) # Optimized inherit weight decay is bloaked. weight decay is computed in this py. 
+
         _check_param_value(beta1, beta2, eps, self.cls_name)
         validator.check_value_type("use_locking", use_locking, [bool], self.cls_name)
 
@@ -202,11 +213,11 @@ class Adan(Optimizer):
         #self.beta3_power = Parameter(initializer(1, [1], mstype.float32), name="beta3_power")
         self.eps = Tensor(eps, mstype.float32)
         self.use_locking = use_locking
-        self.use_amsgrad = use_amsgrad
         self.moment1 = self._parameters.clone(prefix="moment1", init='zeros') # m 
         self.moment2 = self._parameters.clone(prefix="moment2", init='zeros') # v
         self.moment3 = self._parameters.clone(prefix="moment3", init='zeros') # n
-        self.prev_gradient= self._parameters.clone(prefix="prev_gradient", init='zeros') # n
+        self.prev_gradient= self._parameters.clone(prefix="prev_gradient", init='zeros') 
+        # print('prev g: ', type(self.prev_gradient))
        
         self.weight_decay = Tensor(weight_decay, mstype.float32)
 
@@ -241,9 +252,13 @@ class Adan(Optimizer):
         lr = self.get_lr()
         #weight_decay = self.get_weight_decay()
 
+        #if self.global_step == 0:
+        #    success = F.depend(True, F.assign(self.prev_gradient, gradients))
+
         #TODO: currently not support dist 
         success = self.map_(F.partial(_adan_opt, self.beta1, self.beta2, self.beta3, self.eps, lr, self.weight_decay),
                 params, moment1, moment2, moment3, gradients, self.prev_gradient)
+                #params, moment1, moment2, moment3, gradients, self.prev_gradient, self.global_step)
 
 
         return success