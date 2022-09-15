d499419ea499ac781a1266d619a263a8b5a2a079
samithuang <285365963@qq.com>
2022-09-15 10:48:55 +0000

    hide sampler and add seq/rnd sample inside create_dataset

--- mindcv/data/dataset_factory.py
+++ mindcv/data/dataset_factory.py
@@ -4,6 +4,7 @@ import os
 from .dataset_download import MnistDownload, Cifar10Download, Cifar100Download
 
 from mindspore.dataset import MnistDataset, Cifar10Dataset, Cifar100Dataset, ImageFolderDataset
+import mindspore.dataset as ds
 
 _MINDSPORE_BASIC_DATASET = dict(
     mnist=(MnistDataset, MnistDownload),
@@ -16,8 +17,8 @@ def create_dataset(
         name: str = '',
         root: str = './',
         split: str = 'train',
-        shuffle: Optional[bool] = None,
-        sampler=None,
+        shuffle: bool = True,
+        num_samples: Optional[bool] = None,
         num_shards: Optional[int] = None,
         shard_id: Optional[int] = None,
         num_parallel_workers: Optional[int] = None,
@@ -28,7 +29,8 @@ def create_dataset(
     name: dataset name, if empty '' or non-standard dataset name, then process as customized dataset.    
     root: dataset root dir. 
     split: subfolder of root dir, e.g., train, val, test
-
+    shuffle: whether to shuffle the dataset, default: True
+    num_samples (int, optional): Number of elements to sample (default=None, which means sample all elements).
     
     For custom datasets and imagenet, the dataset dir should follow the structure like: 
     .dataset_name/
@@ -52,6 +54,18 @@ def create_dataset(
            └── ....
 
     '''
+    
+    if num_samples == None:
+        sampler = None
+    elif num_samples > 0:
+        if shuffle:
+            sampler = ds.RandomSampler(replacement=False, num_samples=num_samples)
+        else:
+            sampler = ds.SequentialSampler(num_samples=num_samples)
+        shuffle = None # shuffle and sampler cannot be set at the same in mindspore datatset API
+    else:
+        sampler = None
+
     name = name.lower()
     mindspore_kwargs = dict(shuffle=shuffle, sampler=sampler, num_shards=num_shards, shard_id=shard_id,
                             num_parallel_workers=num_parallel_workers, **kwargs)
