849450b57de9810f23ab15ce1d59aaa61aa93cae
samithuang <285365963@qq.com>
2022-09-21 14:21:24 +0000

    add training in step and pynative mode

---
+++ train_pynative.py
@@ -0,0 +1,156 @@
+import mindspore as ms
+from mindspore import FixedLossScaleManager, Model, LossMonitor, TimeMonitor, CheckpointConfig, ModelCheckpoint
+from mindspore.communication import init, get_rank, get_group_size
+
+from mindcv.models import create_model
+from mindcv.data import create_dataset, create_transforms, create_loader
+from mindcv.loss import create_loss
+from mindcv.optim import create_optimizer
+from mindcv.scheduler import create_scheduler
+from config import parse_args
+from time import time
+from mindspore import ops
+import os
+
+ms.set_seed(1)
+
+def train_epoch(network, dataset, loss_fn, optimizer, log_interval=100):
+    # Define forward function
+    def forward_fn(data, label):
+        logits = network(data)
+        loss = loss_fn(logits, label)
+        return loss, logits
+
+    # Get gradient function
+    grad_fn = ops.value_and_grad(forward_fn, None, optimizer.parameters, has_aux=True)
+
+    # Define function of one-step training,
+    @ms.ms_function
+    def train_step(data, label):
+        (loss, _), grads = grad_fn(data, label)
+        loss = ops.depend(loss, optimizer(grads))
+        return loss
+
+    network.set_train()
+    size = dataset.get_dataset_size()
+    for batch, (data, label) in enumerate(dataset.create_tuple_iterator()):
+        loss = train_step(data, label)
+        if (batch+1) % log_interval  == 0:
+            loss, current = loss.asnumpy(), batch
+            print(f"Step {current+1:>3d}/{size:>3d}, loss: {loss:>7f}")
+
+
+def train(args):
+    ms.set_context(mode=ms.PYNATIVE_MODE)
+
+    if args.distribute:
+        init()
+        device_num = get_group_size()
+        rank_id = get_rank()
+        ms.set_auto_parallel_context(device_num=device_num,
+                                     parallel_mode='data_parallel',
+                                     gradients_mean=True)
+    else:
+        device_num = None
+        rank_id = None
+
+    # create dataset
+    dataset_train = create_dataset(
+        name=args.dataset,
+        root=args.data_dir,
+        split=args.train_split,
+        shuffle=args.shuffle,
+        num_samples=args.num_samples,
+        num_shards=device_num,
+        shard_id=rank_id,
+        num_parallel_workers=args.num_parallel_workers,
+        download=args.dataset_download)
+
+    # create transforms
+    transform_list = create_transforms(
+        dataset_name=args.dataset,
+        is_training=True,
+        image_resize=args.image_resize,
+        scale=args.scale,
+        ratio=args.ratio,
+        hflip=args.hflip,
+        vflip=args.vflip,
+        color_jitter=args.color_jitter,
+        interpolation=args.interpolation,
+        auto_augment=args.auto_augment,
+        mean=args.mean,
+        std=args.std,
+        re_prob=args.re_prob,
+        re_scale=args.re_scale,
+        re_ratio=args.re_ratio,
+        re_value=args.re_value,
+        re_max_attempts=args.re_max_attempts
+    )
+
+    # load dataset
+    loader_train = create_loader(
+        dataset=dataset_train,
+        batch_size=args.batch_size,
+        drop_remainder=False,
+        is_training=True,
+        mixup=args.mixup,
+        num_classes=args.num_classes,
+        transform=transform_list,
+        num_parallel_workers=args.num_parallel_workers,
+    )
+    steps_per_epoch = loader_train.get_dataset_size()
+
+    # create model
+    network = create_model(model_name=args.model,
+                           num_classes=args.num_classes,
+                           in_channels=args.in_channels,
+                           drop_rate=args.drop_rate,
+                           drop_path_rate=args.drop_path_rate,
+                           pretrained=args.pretrained,
+                           checkpoint_path=args.ckpt_path)
+
+    # create loss
+    loss = create_loss(name=args.loss,
+                       reduction=args.reduction,
+                       label_smoothing=args.label_smoothing,
+                       aux_factor=args.aux_factor)
+
+    # create learning rate schedule
+    lr_scheduler = create_scheduler(steps_per_epoch,
+                                    scheduler=args.scheduler,
+                                    lr=args.lr,
+                                    min_lr=args.min_lr,
+                                    warmup_epochs=args.warmup_epochs,
+                                    decay_epochs=args.decay_epochs,
+                                    decay_rate=args.decay_rate)
+
+    # create optimizer
+    optimizer = create_optimizer(network.trainable_params(),
+                                 opt=args.opt,
+                                 lr=lr_scheduler,
+                                 weight_decay=args.weight_decay,
+                                 momentum=args.momentum,
+                                 nesterov=args.use_nesterov,
+                                 filter_bias_and_bn=args.filter_bias_and_bn,
+                                 loss_scale=args.loss_scale)
+
+    # training
+    # TODO: args.loss_scale is not making effect. 
+    print('Training...')
+    for t in range(args.epoch_size):
+        #print(f"Epoch {t+1}\n-------------------------------")
+        b = time()
+        train_epoch(network, loader_train, loss, optimizer, log_interval=steps_per_epoch)
+        print(f'Epoch {t+1} training time: {time()-b: .3f}s')
+
+	# Save checkpoint
+        if ((t + 1) % args.ckpt_save_interval == 0):
+            if (not args.distribute) or (args.distribute and rank_id==0):
+                save_path = os.path.join(args.ckpt_path, f"{args.model}-{t+1}.ckpt")
+                ms.save_checkpoint(network, save_path, async_save=True)
+                print(f"Saving model to {save_path}")
+    print("Done!")
+
+if __name__ == '__main__':
+    args = parse_args()
+    train(args)
