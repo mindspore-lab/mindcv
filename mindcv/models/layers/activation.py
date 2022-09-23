547ed0083cae8b5db6ad88b2ad955824c0caeb0c
xiuyu0000 <110210055+xiuyu0000@users.noreply.github.com>
2022-09-23 15:21:16 +0800

    Update activation.py

--- mindcv/models/layers/activation.py
+++ mindcv/models/layers/activation.py
@@ -3,7 +3,6 @@ Custom operators.
 """
 
 from mindspore import nn
-from mindspore import ops
 
 __all__ = ['Swish']
 
@@ -26,15 +25,13 @@ class Swish(nn.Cell):
     def __init__(self):
         super(Swish, self).__init__()
         self.result = None
-        self.sigmoid = ops.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
 
     def construct(self, x):
-        """ construct swish """
         result = x * self.sigmoid(x)
         return result
 
     def bprop(self, x, dout):
-        """ bprop """
         sigmoid_x = self.sigmoid(x)
         result = dout * (sigmoid_x * (1 + x * (1 - sigmoid_x)))
         return result
