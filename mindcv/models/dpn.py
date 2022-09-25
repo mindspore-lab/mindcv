3dab65600de24270a4b45572f58d52a9cdb5995f
JunyuLiu-AI <100194830+JunyuLiu1@users.noreply.github.com>
2022-09-26 03:22:56 +0800

    Update dpn.py

--- mindcv/models/dpn.py
+++ mindcv/models/dpn.py
@@ -30,7 +30,7 @@ def _cfg(url='', **kwargs):
     return {
         'url': url,
         'num_classes': 1000,
-        'first_conv': '', 'classifier': '',
+        'first_conv': 'features.conv1.conv', 'classifier': 'classifier',
         **kwargs
     }
 
