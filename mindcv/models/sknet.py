8f3425531d69c9d31c6d42fa13a7311f13313b31
JunyuLiu-AI <100194830+JunyuLiu1@users.noreply.github.com>
2022-09-26 04:14:17 +0800

    Update sknet.py

--- mindcv/models/sknet.py
+++ mindcv/models/sknet.py
@@ -26,7 +26,7 @@ def _cfg(url='', **kwargs):
     return {
         'url': url,
         'num_classes': 1000,
-        'first_conv': '', 'classifier': '',
+        'first_conv': 'conv1', 'classifier': 'classifier',
         **kwargs
     }
 
