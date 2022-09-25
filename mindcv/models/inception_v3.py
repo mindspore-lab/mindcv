6a9cea5d63c559e99a66358e2972fcf525ae6951
JunyuLiu-AI <100194830+JunyuLiu1@users.noreply.github.com>
2022-09-26 03:37:38 +0800

    Update inception_v3.py

--- mindcv/models/inception_v3.py
+++ mindcv/models/inception_v3.py
@@ -24,7 +24,7 @@ def _cfg(url='', **kwargs):
     return {
         'url': url,
         'num_classes': 1000,
-        'first_conv': '', 'classifier': '',
+        'first_conv': 'conv1a', 'classifier': 'classifier',
         **kwargs
     }
 
