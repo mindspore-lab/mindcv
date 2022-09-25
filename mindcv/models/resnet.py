a3de3b79ed82c9c60cda06778fe0719d335df063
JunyuLiu-AI <100194830+JunyuLiu1@users.noreply.github.com>
2022-09-26 04:08:12 +0800

    Update resnet.py

--- mindcv/models/resnet.py
+++ mindcv/models/resnet.py
@@ -30,7 +30,7 @@ def _cfg(url='', **kwargs):
     return {
         'url': url,
         'num_classes': 1000,
-        'first_conv': '', 'classifier': '',
+        'first_conv': 'conv1', 'classifier': 'classifier',
         **kwargs
     }
 
