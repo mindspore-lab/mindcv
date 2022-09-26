f60a270799681c9c5c191e3053fe63ebea4f569d
JunyuLiu-AI <100194830+JunyuLiu1@users.noreply.github.com>
2022-09-26 14:35:30 +0800

    Update inception_v3.py

--- mindcv/models/inception_v3.py
+++ mindcv/models/inception_v3.py
@@ -30,7 +30,7 @@ def _cfg(url='', **kwargs):
 
 
 default_cfgs = {
-    'inception_v3': _cfg(url='')
+    'inception_v3': _cfg(url='https://download.mindspore.cn/toolkits/mindcv/inception/inception_v3_299.ckpt')
 }
 
 
