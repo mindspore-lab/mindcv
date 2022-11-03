e4cb0d83706b4dbfeb707a2577935f26346590d0
Genius Patrick <yufeiwang.work@outlook.com>
2022-11-03 19:54:47 +0800

    fix installation.

--- setup.py
+++ setup.py
@@ -1,92 +1,42 @@
 #!/usr/bin/env python
 
-import os
-import shutil
-import stat
-
-from setuptools import find_packages
-from setuptools import setup
-from setuptools.command.build_py import build_py
-from setuptools.command.egg_info import egg_info
-
+from setuptools import setup, find_packages
 
 exec(open('mindcv/version.py').read())
-version = __version__
-package_name = 'mindcv'
-cur_dir = os.path.dirname(os.path.realpath(__file__))
-pkg_dir = os.path.join(cur_dir, 'build')
-
-
-def clean():
-    # pylint: disable=unused-argument
-    def readonly_handler(func, path, execinfo):
-        os.chmod(path, stat.S_IWRITE)
-        func(path)
-
-    if os.path.exists(os.path.join(cur_dir, 'build')):
-        shutil.rmtree(os.path.join(cur_dir, 'build'), onerror=readonly_handler)
-    if os.path.exists(os.path.join(cur_dir, f'{package_name}.egg-info')):
-        shutil.rmtree(os.path.join(cur_dir, f'{package_name}.egg-info'), onerror=readonly_handler)
-
-
-clean()
-
-
-def update_permissions(path):
-    """
-    Update permissions.
-
-    Args:
-        path (str): Target directory path.
-    """
-    for dirpath, dirnames, filenames in os.walk(path):
-        for dirname in dirnames:
-            dir_fullpath = os.path.join(dirpath, dirname)
-            os.chmod(dir_fullpath, stat.S_IREAD | stat.S_IWRITE | stat.S_IEXEC | stat.S_IRGRP | stat.S_IXGRP)
-        for filename in filenames:
-            file_fullpath = os.path.join(dirpath, filename)
-            os.chmod(file_fullpath, stat.S_IREAD)
-
-
-class EggInfo(egg_info):
-    """Egg info."""
-
-    def run(self):
-        super().run()
-        egg_info_dir = os.path.join(cur_dir, f'{package_name}.egg-info')
-        update_permissions(egg_info_dir)
-
-
-class BuildPy(build_py):
-    """BuildPy."""
-
-    def run(self):
-        super().run()
-        mindarmour_dir = os.path.join(pkg_dir, 'lib', package_name)
-        update_permissions(mindarmour_dir)
-
 
 setup(
-    name=package_name,
-    version=version,
-    author="MindLab-AI",
-    url="https://github.com/mindlab-ai/mindcv",
+    name="mindcv",
+    author="MindSpore Ecosystem",
+    author_email='mindspore-ecosystem@example.com',
+    url="https://github.com/mindspore-ecosystem/mindcv",
     project_urls={
-        'Sources': 'https://github.com/mindlab-ai/mindcv',
-        'Issue Tracker': 'https://github.com/mindlab-ai/mindcv/issues',
+        'Sources': 'https://github.com/mindspore-ecosystem/mindcv',
+        'Issue Tracker': 'https://github.com/mindspore-ecosystem/mindcv/issues',
     },
-    description="An open source computer vision research tool box.",
-    license='Apache 2.0',
+    description="A toolbox of vision models and algorithms based on MindSpore.",
+    license="Apache Software License 2.0",
     include_package_data=True,
-    packages=find_packages(exclude=("mindcv")),
-    cmdclass={
-        'egg_info': EggInfo,
-        'build_py': BuildPy,
-    },
+    packages=find_packages(include=['mindcv', 'mindcv.*']),
     install_requires=[
         'numpy >= 1.17.0',
         'PyYAML >= 5.3',
         'tqdm'
-    ]
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=[
+        'pytest',
+    ],
+    version=__version__,
+    zip_safe=False
 )
-print(find_packages())
