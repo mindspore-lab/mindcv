455d138693e1c2792210ceb1124541c31ef1d694
samithuang <285365963@qq.com>
2022-12-06 16:38:37 +0000

    add repeated augmentation

--- mindcv/data/dataset_factory.py
+++ mindcv/data/dataset_factory.py
@@ -6,10 +6,12 @@ from typing import Optional
 import os
 
 from mindspore.dataset import MnistDataset, Cifar10Dataset, Cifar100Dataset, ImageFolderDataset, DistributedSampler
-import mindspore.dataset as ds
 
 from .dataset_download import MnistDownload, Cifar10Download, Cifar100Download
-from .distributed_sampler import RepAugDistributedSampler
+from .distributed_sampler import RepeatAugSampler
+
+#import mindspore.dataset as ds
+#from .dataset_reader import ImageNetDataset
 
 __all__ = ["create_dataset"]
 
@@ -19,6 +21,7 @@ _MINDSPORE_BASIC_DATASET = dict(
     cifar100=(Cifar100Dataset, Cifar100Download)
 )
 
+#_DATASET_SIZE = {'imagenet': }
 
 def create_dataset(
         name: str = '',
@@ -30,6 +33,7 @@ def create_dataset(
         shard_id: Optional[int] = None,
         num_parallel_workers: Optional[int] = None,
         download: bool = False,
+        num_aug_repeats: int = 0,
         **kwargs
 ):
     r"""Creates dataset by name.
@@ -47,6 +51,7 @@ def create_dataset(
             This argument can only be specified when `num_shards` is also specified.
         num_parallel_workers: Number of workers to read the data (default=None, set in the config).
         download: whether to download the dataset. Default: False
+        num_aug_repeats: Number of dataset repeatition for repeated augmentation. If 0 or 1, repeated augmentation is diabled. Otherwise, repeated augmentation is enabled and the common choice is 3. (Default: 0)
 
     Note:
         For custom datasets and imagenet, the dataset dir should follow the structure like:
@@ -73,10 +78,16 @@ def create_dataset(
     Returns:
         Dataset object
     """
-    name = name.lower()
 
+    assert (num_samples is None) or (num_aug_repeats==0), 'num_samples and num_aug_repeats can NOT be set together.'
+
+    name = name.lower()
+    # subset sampling
     if num_samples is not None and num_samples > 0:
+        num_shards = 1 if num_shards is None else num_shards
+        shard_id = 0 if shard_id is None else shard_id
         sampler = DistributedSampler(num_shards, shard_id, shuffle=shuffle, num_samples=num_samples)
+
         shuffle = None  # shuffle and sampler cannot be set at the same in mindspore datatset API
         mindspore_kwargs = dict(shuffle=shuffle, sampler=sampler,
                             num_parallel_workers=num_parallel_workers, **kwargs)
@@ -85,6 +96,15 @@ def create_dataset(
         mindspore_kwargs = dict(shuffle=shuffle, sampler=sampler, num_shards=num_shards, shard_id=shard_id,
                             num_parallel_workers=num_parallel_workers, **kwargs)
 
+    # sampler for repeated augmentation
+    if num_aug_repeats > 0:
+        dataset_size = get_dataset_size(name, root, split)
+        print(f'INFO: Repeated augmentation is enabled, num_aug_repeats: {num_aug_repeats}, original dataset size: ', dataset_size)
+        #since drop_remainder is usally True, we don't need to do rounding in sampling
+        sampler = RepeatAugSampler(dataset_size, num_shards=num_shards, rank_id=shard_id, num_repeats=num_aug_repeats, selected_round=0, shuffle=shuffle)
+        mindspore_kwargs = dict(shuffle=None, sampler=sampler, num_shards=None, shard_id=None, **kwargs)
+
+    # create dataset
     if name in _MINDSPORE_BASIC_DATASET:
         dataset_class = _MINDSPORE_BASIC_DATASET[name][0]
         dataset_download = _MINDSPORE_BASIC_DATASET[name][1]
@@ -114,5 +134,21 @@ def create_dataset(
         if os.path.isdir(root):
             root = os.path.join(root, split)
         dataset = ImageFolderDataset(dataset_dir=root, **mindspore_kwargs)
-
+        ''' Another implementation which a bit slower than ImageFolderDataset
+            imagenet_dataset = ImageNetDataset(dataset_dir=root)
+            # TODO: why round by 256?
+            sampler = RepeatAugSampler(len(imagenet_dataset), num_shards=num_shards, rank_id=shard_id, num_repeats=repeated_aug, selected_round=1, shuffle=shuffle)
+            dataset = ds.GeneratorDataset(imagenet_dataset, column_names=imagenet_dataset.column_names, sampler=sampler)
+        '''
     return dataset
+
+def get_dataset_size(name, root, split):
+    if name in _MINDSPORE_BASIC_DATASET:
+        dataset_class = _MINDSPORE_BASIC_DATASET[name][0]
+        dataset = dataset_class(dataset_dir=root, usage=split)
+    else:
+        if os.path.isdir(root):
+            root = os.path.join(root, split)
+        dataset = ImageFolderDataset(dataset_dir=root)
+
+    return dataset.get_dataset_size()
