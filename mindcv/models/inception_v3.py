fbb165b748a8543bc0c632db0fff85845253dc09
Andre <821372701@qq.com>
2022-12-29 20:41:12 +0800

    Delete inceptionv3 transfer weights

--- mindcv/models/inception_v3.py
+++ mindcv/models/inception_v3.py
@@ -28,7 +28,7 @@ def _cfg(url='', **kwargs):
 
 
 default_cfgs = {
-    'inception_v3': _cfg(url='https://download.mindspore.cn/toolkits/mindcv/inception/inception_v3_299.ckpt')
+    'inception_v3': _cfg(url='')
 }
 
 
