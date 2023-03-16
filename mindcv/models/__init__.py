1a9311eb6b4e79782f2f77b63428ae8a1f1ab0b3
xusheng365 <73642049+xusheng365@users.noreply.github.com>
2023-03-16 17:02:08 +0800

    [Feature] add model script, training recipe and trained weights of CrossViT (#523)

--- mindcv/models/__init__.py
+++ mindcv/models/__init__.py
@@ -3,6 +3,7 @@ from . import (
     bit,
     convit,
     convnext,
+    crossvit,
     densenet,
     dpn,
     edgenext,
@@ -46,6 +47,7 @@ from . import (
 from .bit import *
 from .convit import *
 from .convnext import *
+from .crossvit import *
 from .densenet import *
 from .dpn import *
 from .edgenext import *
@@ -93,6 +95,7 @@ __all__ = []
 __all__.extend(bit.__all__)
 __all__.extend(convit.__all__)
 __all__.extend(convnext.__all__)
+__all__.extend(crossvit.__all__)
 __all__.extend(densenet.__all__)
 __all__.extend(dpn.__all__)
 __all__.extend(edgenext.__all__)
