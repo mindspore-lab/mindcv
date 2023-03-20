ab140d01dcc95415df5f4f5c516acd409736185e
Samit <285365963@qq.com>
2023-03-20 10:50:12 +0800

    Fix classifier_name bug in load_pretrain function

--- mindcv/models/utils.py
+++ mindcv/models/utils.py
@@ -47,7 +47,7 @@ def load_pretrained(model, default_cfg, num_classes=1000, in_channels=3, filter_
 
     classifier_name = default_cfg["classifier"]
     if num_classes == 1000 and default_cfg["num_classes"] == 1001:
-        classifier_weight = param_dict["classifier_name" + ".weight"]
+        classifier_weight = param_dict[classifier_name + ".weight"]
         classifier_weight.set_data(classifier_weight[:1000], slice_shape=True)
         classifier_bias = param_dict[classifier_name + ".bias"]
         classifier_bias.set_data(classifier_bias[:1000], slice_shape=True)
