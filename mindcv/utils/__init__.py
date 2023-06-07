7573837823db50b90b940c7fffffa2e1d503167f
Cheung Ka Wai <zhtmike@gmail.com>
2023-06-07 18:01:44 +0800

    feat: support gradient accumulation (#609)

--- mindcv/utils/__init__.py
+++ mindcv/utils/__init__.py
@@ -3,6 +3,7 @@ from .amp import *
 from .callbacks import *
 from .checkpoint_manager import *
 from .download import *
+from .gradient_accumulation import *
 from .path import *
 from .random import *
 from .reduce_manager import *
--- train.py
+++ train.py
@@ -224,6 +224,7 @@ def train(args):
         ema_decay=args.ema_decay,
         clip_grad=args.clip_grad,
         clip_value=args.clip_value,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
     )
 
     # callback
