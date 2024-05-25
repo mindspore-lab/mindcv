517fa99162946180528fceabb597a63ac82f3ac8
Pingqi Li <58093835+PingqiLi@users.noreply.github.com>
2024-05-25 11:25:20 +0800

    Make ssd data module compatible with mindspore v2.0+ version  (#778)
    * fix the config of squeezenet_1.0 with amp_level O0
    
    * Modify the ssd/data.py to make it compatible with the mindspore v2.0+ version.

--- examples/det/ssd/data.py
+++ examples/det/ssd/data.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 
 import cv2
@@ -175,14 +176,21 @@ def create_ssd_dataset(
             output_columns = ["img_id", "image", "image_shape"]
             trans = [normalize_op, change_swap_op]
 
+        # Note: mindspore-2.0 delete the parameter column_order
+        sig = inspect.signature(ds.map)
+        pass_column_order = False if "kwargs" in sig.parameters else True
+
         ds = ds.map(
             operations=compose_map_func,
             input_columns=["img_id", "image", "annotation"],
             output_columns=output_columns,
-            column_order=output_columns,
+            column_order=output_columns if pass_column_order else None,
             python_multiprocessing=python_multiprocessing,
             num_parallel_workers=num_parallel_workers,
         )
+        if not pass_column_order:
+            ds.project(output_columns=output_columns)
+
         ds = ds.map(
             operations=trans,
             input_columns=["image"],
